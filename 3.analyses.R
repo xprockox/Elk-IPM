123a65187c84091b18b5a9cb58c8ce40fb209d78
xprockox <xprockox@gmail.com>
2025-06-09 16:49:31 -0600

    truncated priors, allowed 0 obs error, new data list

--- 3.analyses.R
+++ 3.analyses.R
@@ -12,6 +12,9 @@ library(MCMCvis)
 ### data loading
 modeling_df <- read.csv('data/intermediate/modeling_df.csv')
 
+# let's try removing the last five years because they are all NAs
+modeling_df <- modeling_df[1:26,]
+
 ############################################################################################
 ### model structure
 
@@ -24,17 +27,17 @@ elk_ipm1 <- nimbleCode({
   
   # priors for time-varying fecundity and survival
   for (t in 1:(nYears - 1)) {
-    f[t] ~ T(dnorm(fem_birth[t], 1), 0, )  # fecundity with truncation
+    f[t] ~ T(dnorm(fem_birth[t], 0.25), 0, ) # fecundity with truncation
     
-    phi_calf[t] ~ T(dnorm(surv_calf[t], 0.25), 0, 1)  # calf survival
-    phi_ya[t] ~ T(dnorm(surv_ya[t], 0.25), 0, 1)      # young adult survival
-    phi_oa[t] ~ T(dnorm(surv_oa[t], 0.25), 0, 1)      # old adult survival
+    phi_calf[t] ~ T(dnorm(surv_calf[t], 0.5), 0, 1) # calf survival
+    phi_ya[t] ~ T(dnorm(surv_ya[t], 0.5), 0, 1) # young adult survival
+    phi_oa[t] ~ T(dnorm(surv_oa[t], 0.5), 0, 1) # old adult survival
     
     psi[t] ~ dbeta(1, 1) # transition prob. from ya to oa
   }
   
   # obs error
-  sigma_obs ~ dunif(2, 25)
+  sigma_obs ~ dunif(0, 25)
   
   # process model
   for (t in 1:(nYears - 1)) {
@@ -74,41 +77,42 @@ nYears <- nrow(modeling_df) # define here first so it can be used in inits
 
 # informative initial values based on 1995 proportions
 N_calf_1 <- modeling_df$Total_Elk_Female[1] * modeling_df$Percent.N.calves[1]
-N_ya_1   <- modeling_df$Total_Elk_Female[1] * modeling_df$Percent.N.prime[1]
-N_oa_1   <- modeling_df$Total_Elk_Female[1] * modeling_df$Percent.N.old[1]
+N_ya_1 <- modeling_df$Total_Elk_Female[1] * modeling_df$Percent.N.prime[1]
+N_oa_1 <- modeling_df$Total_Elk_Female[1] * modeling_df$Percent.N.old[1]
 
 # data
 data <- list(
   y = modeling_df$Total_Elk_Female,
-  fem_birth = ifelse(is.na(modeling_df$FemBirthPerCow[1:(nYears - 1)]), 
-                     mean(modeling_df$FemBirthPerCow[1:(nYears - 1)], na.rm = TRUE), 
-                     modeling_df$FemBirthPerCow[1:(nYears - 1)]),
-  surv_calf = ifelse(is.na(modeling_df$Survival_calf[1:(nYears - 1)]), 
-                     mean(modeling_df$Survival_calf[1:(nYears - 1)], na.rm = TRUE), 
-                     modeling_df$Survival_calf[1:(nYears - 1)]),
-  surv_ya = ifelse(is.na(modeling_df$Survival_prime[1:(nYears - 1)]), 
-                   mean(modeling_df$Survival_prime[1:(nYears - 1)], na.rm = TRUE), 
-                   modeling_df$Survival_prime[1:(nYears - 1)]),
-  surv_oa = ifelse(is.na(modeling_df$Survival_old[1:(nYears - 1)]), 
-                   mean(modeling_df$Survival_old[1:(nYears - 1)], na.rm = TRUE), 
-                   modeling_df$Survival_old[1:(nYears - 1)])
+  fem_birth = modeling_df$FemBirthPerCow[1:(nYears - 1)],
+  surv_calf = modeling_df$Survival_calf[1:(nYears - 1)],
+  surv_ya = modeling_df$Survival_prime[1:(nYears - 1)],
+  surv_oa = modeling_df$Survival_old[1:(nYears - 1)]
 )
 
 # constants
 constants <- list(
   nYears = nYears,
   mu_calf_1 = N_calf_1,
-  mu_ya_1   = N_ya_1,
-  mu_oa_1   = N_oa_1
+  mu_ya_1 = N_ya_1,
+  mu_oa_1 = N_oa_1
 )
 
 # initial values
 inits <- list(
+  # abundances
   N_calf = pmax(1, round(rnorm(nYears, mean = N_calf_1, sd = N_calf_1 / 5))),
-  N_ya   = pmax(1, round(rnorm(nYears, mean = N_ya_1, sd = N_ya_1 / 5))),
-  N_oa   = pmax(1, round(rnorm(nYears, mean = N_oa_1, sd = N_oa_1 / 5))),
+  N_ya = pmax(1, round(rnorm(nYears, mean = N_ya_1, sd = N_ya_1 / 5))),
+  N_oa = pmax(1, round(rnorm(nYears, mean = N_oa_1, sd = N_oa_1 / 5))),
+  
+  # vital rates
+  f = pmax(0.01, rnorm(nYears - 1, mean = mean(data$fem_birth), sd = 0.05)),
+  phi_calf = pmin(pmax(rnorm(nYears - 1, mean = mean(data$surv_calf), sd = 0.05), 0.01), 0.99),
+  phi_ya = pmin(pmax(rnorm(nYears - 1, mean = mean(data$surv_ya), sd = 0.05), 0.01), 0.99),
+  phi_oa = pmin(pmax(rnorm(nYears - 1, mean = mean(data$surv_oa), sd = 0.05), 0.01), 0.99),
   psi = rep(0.08, nYears - 1),
-  sigma_obs = 5
+  
+  # obs error
+  sigma_obs = round(runif(1, 0, 10))
 )
 
 # parameters to estimate
